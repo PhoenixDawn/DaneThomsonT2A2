48cabd8fd4b7aaddc3dd07cab42eedad70cbd481
Dane Thomson <dane.thomson@outlook.com>
2021-05-13 16:18:43 +1000

    Reworked database

--- db/schema.rb
+++ db/schema.rb
@@ -39,12 +39,12 @@ ActiveRecord::Schema.define(version: 2021_05_13_035716) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
