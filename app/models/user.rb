6e77f97daf9a37b0a7c9aa3e4c43e8ca45798335
Dane Thomson <dane.thomson@outlook.com>
2021-05-19 14:50:55 +1000

    Added more styling, added user name to messages so you know how you're talking to

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,15 @@ class User < ApplicationRecord
   rolify
   acts_as_messageable
 
+  before_destroy { messages.destroy_all }
+
+  has_many :listings, dependent: :destroy
+
   validates :street_number, presence: true
   validates :postcode, presence: true
   validates :suburb, presence: true
   validates :state, presence: true
+  validates :name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Role.destroy_all
 
-Role.create(name: "admin")
-Role.create(name: "grandma")
-Role.create(name: "user")
+User.destroy_all
+
+grannyG = User.create(email: "a@b.c", password: "123456", street_number: "69 - 75 Duncan Road", postcode: "4157", suburb: "Sheldon", state: "QLD", name: "Granny G")
+grannyB = User.create(email: "a@b.com", password: "123456", street_number: "69 - 75 Duncan Road", postcode: "4157", suburb: "Sheldon", state: "QLD", name: "Granny B")
+
+grannyG.add_role(:grandma)
